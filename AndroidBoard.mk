bdf9352b43cbf94f5908eb6bc864be7914325f80
Asure <asure007@gmail.com>
2010-10-31 14:33:50 +0100

    Resynched commit preparing for pull.

---
+++ AndroidBoard.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+endif
+
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_RECOVERY_ROOT_OUT)/sbin/postrecoveryboot.sh
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/postrecoveryboot.sh | $(ACP)
+	$(transform-prebuilt-to-target)
+	
